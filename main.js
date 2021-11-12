c4fdaf32602186232e96792ddbe001e94e719b94
Lucas Barbosa <84606792+lucasbarbosa1@users.noreply.github.com>
2021-11-12 13:24:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        connected: false,
+        ros: null,
+        ws_address: 'ws://localhost:9090',
+        logs: [],
+        loading: false,
+        topic: null,
+        message: null,
+    },
+    methods: {
+        connect: function() {
+            this.loading = true
+            this.ros = new ROSLIB.Ros({
+                url: this.ws_address
+            })
+            this.ros.on('connection', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+                this.connected = true
+                this.loading = false
+                this.setCamera()
+            })
+            this.ros.on('error', (error) => {
+                this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
+            })
+            this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+                this.connected = false
+                this.loading = false
+                
+            })
+        },
+        disconnect: function() {
+            this.ros.close()
+        },
+        /*setTopic: function() {
+            this.topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+        },
+        forward: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        stop: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        backward: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: -1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        turnLeft: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.5, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0.5, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        turnRight: function() {
+            this.message = new ROSLIB.Message({
+                linear: { x: 0.5, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: -0.5, },
+            })
+            this.setTopic()
+            this.topic.publish(this.message)
+        },
+        */
+        setCamera: function() {
+            this.cameraViewer = new MJPEGCANVAS.Viewer({
+                divID: 'mjpeg',
+                host: 'http://localhost:8080/stream?topic=/image_front_cam',
+                width: 640,
+                height: 480,
+                topic: '/camera/image',
+                port: 5744,
+            })
+        },
+    },
+    mounted() {
+    },
+})
